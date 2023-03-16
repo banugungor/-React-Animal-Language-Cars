03aca73b2ec3a1c8192a2d17b55a0c80e7d2a923
banu <nbanu_uzun@hotmail.com>
2023-03-16 13:17:03 +0300

    edit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import Container from "react-bootstrap/Container";
 import CardContainer from "./components/CardContainer";
 import Header from "./components/Header";
-import WordsCard from "./components/WordsCard";
 
 function App() {
   return (
